656b6b321809707974be2bb8261c64fe14f091f1
Dmitrijs Firosvs <125821414+DmitrijsFIrsovs@users.noreply.github.com>
2023-04-16 17:29:38 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,47 +1,47 @@
-# python3
-import sys
-
+# Python 3
 
 def read_input():
-    if len(sys.argv) > 1 and sys.argv[1] == 'F':
-        with open(sys.argv[2], "r") as f:
-            pattern = f.readline().strip()
-            text = f.readline().strip()
     
+
+    input_type = input().rstrip()
+    if input_type == 'I':
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif input_type == 'F':       
+        filename = input().rstrip()
+        with open(filename) as f:
+            pattern = f.readline().rstrip()
+            text = f.readline().rstrip()
     else:
-        pattern = input().strip()
-        text = input().strip()
-       
-    return pattern , text
+        raise ValueError('Invalid input type')
+
+    return pattern, text
 
 def print_occurrences(output):
-  
+    
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    p = 1000000007
-    x = 263
+    
 
     
-    g = sum([ord(pattern[i]) * x**i for i in range(len(pattern))]) % p
-    h = sum([ord(text[i]) * x**i for i in range(len(pattern))]) % p
-                   
-    pos = []                
-   
-    for i in range( len(text), len(pattern) + 1):
-        if  g == h and text[i:i + len(pattern)] == pattern:
-             pos.append(i)
-        if i < len(text) - len(pattern):
-            hash = 0
-            for j in range(len(pattern)):
-                hash += ord(text[i+j+1]) * x**j
-            h =  (h - ord(text[i]) * x**(len(pattern)-1))  * x + hash    
-            h = h % p
-                  
+    pattern_len = len(pattern)
+    text_len = len(text)
+    pattern_hash = hash(pattern)
+    text_hash = hash(text[:pattern_len])
+    pos = []
+
+    
+    for i in range(text_len - pattern_len + 1):   
+        if pattern_hash == text_hash:   
+            if pattern == text[i:i+pattern_len]:
+                pos.append(i)
+
+        
+        if i < text_len - pattern_len:
+            text_hash = hash(text[i+1:i+1+pattern_len])
+
     return pos
-
-
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-

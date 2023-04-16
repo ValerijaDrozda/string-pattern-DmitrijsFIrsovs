bf788ab0f043182ab671c1664ce27ddafc013995
Dmitrijs Firosvs <125821414+DmitrijsFIrsovs@users.noreply.github.com>
2023-04-16 19:09:12 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,49 +1,58 @@
-import os
+# python3
 
 def read_input():
-    try:
-        input_type = input().rstrip()
-        if input_type == 'I':
-            pattern = input().rstrip()
-            text = input().rstrip()
-        elif input_type == 'F':
-            filename = input().rstrip()
-            with open(filename) as f:
-                pattern = f.readline().rstrip()
-                text = f.readline().rstrip()
-        else:
-            raise ValueError('Invalid input type')
-    except EOFError:
-        print("Error: No input provided.")
-        pattern = ''
-        text = ''
+    input_type = input().rstrip()  
+    if input_type == 'i':
         
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif input_type == 'f':
+       
+        file_name = input().rstrip()
+        with open(file_name, 'r') as file:
+            pattern = file.readline().rstrip()
+            text = file.readline().rstrip()
+    else:
+        raise ValueError("Invalid input type")
+
     return pattern, text
+    # this function needs to aquire input both from keyboard and file
+    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    
+    
+    # after input type choice
+    # read two lines 
+    # first line is pattern 
+    # second line is text in which to look for pattern 
+    
+    # return both lines in one return
+    
+    # this is the sample return, notice the rstrip function
+    
+
+def print_occurrences(output):
+    # this function should control output, it doesn't need any return
+    print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    pattern_len = len(pattern)
-    text_len = len(text)
-    pattern_hash = hash(pattern)
-    text_hash = hash(text[:pattern_len])
-    pos = []
+    p = len(pattern)
+    t = len(text)
+    p_hash = hash(pattern)
+    t_hash = hash(text[:p])
+    result = []
 
-    for i in range(text_len - pattern_len + 1):
-        if pattern_hash == text_hash:
-            if pattern == text[i:i + pattern_len]:
-                pos.append(i)
+    for i in range(t - p + 1):
+        if p_hash == t_hash and text[i:i+p] == pattern:
+            result.append(i)
+        if i < t - p:
+            t_hash = hash(text[i+1:i+p+1])
+    return result
+    # this function should find the occurances using Rabin Karp alghoritm 
 
-        if i < text_len - pattern_len:
-            text_hash = hash(text[i + 1:i + 1 + pattern_len])
-
-    return pos
-
-if __name__ == '__main__':
-    pattern, text = read_input()
-    pos = get_occurrences(pattern, text)
-    output_str = ' '.join(map(str, pos))
-    print(output_str)
-    
-    # Write output to environment file
-    with open(os.environ['GITHUB_ENV'], 'a') as f:
-        f.write(f'OUTPUT_STRING={output_str}\n')
+    # and return an iterable variable
    
+
+
+# this part launches the functions
+if __name__ == '__main__':
+    print_occurrences(get_occurrences(*read_input()))
